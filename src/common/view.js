44979e1eb5dcfb867f060254e664ffb2e564411f
Muhammad Zhebarov <xarcho.ev@gmail.com>
2023-05-08 13:00:53 +0300

    add library 'on-change'

---
+++ src/common/view.js
@@ -0,0 +1,16 @@
+export class AbstractView {
+    constructor() {
+        this.app = document.getElementById('root');
+    }
+
+    setTitle(title) {
+        document.title = title;
+    }
+
+    render () {
+        return;
+    }
+    destroy() {
+        return;
+    }
+}
